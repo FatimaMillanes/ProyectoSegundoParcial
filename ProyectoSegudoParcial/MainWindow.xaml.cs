f51a1bee0aa07f44d647092aa22fb36980f20e1f
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-10-04 08:54:23 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoSegudoParcial/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoSegudoParcial
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+           
+                btnGuardar.Visibility = Visibility.Visible;
+                btnCancelar.Visibility = Visibility.Visible;
+                gridInfo.Children.Clear();
+                switch (cbItems.SelectedIndex)
+
+                {
+                    case 0:
+
+                        gridInfo.Children.Add(new ParametrosCafes());
+                        break;
+
+                    case 1:
+
+                        gridInfo.Children.Add(new ParamentrosTes());
+                        break;
+                }
+            
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            gridInfo.Children.Clear();
+            btnGuardar.Visibility = Visibility.Hidden;
+            btnCancelar.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            gridInfo.Children.Clear();
+            btnGuardar.Visibility = Visibility.Hidden;
+            btnCancelar.Visibility = Visibility.Hidden;
+        }
+    }
+}
